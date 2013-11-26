9d6caaf89c9fc0e5a9ecf64431dfb0f7292389ec
Manuel Beta <juanman234@gmail.com>
2013-11-27 07:43:14 +1100

    myob CRUD started

--- myob/2_myob_db_gui_customersCRUD.py
+++ myob/2_myob_db_gui_customersCRUD.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
+# FIX, PLEASE URL ON CONFIG FILE!
+
 #TODO, place hud loading!
+
+#TODO, SEPARATE MYOB CRUD ON OWN CLASS! COMO dbSchema es crud, pero crud para MYOB!
 
 #TEMPLATE CODING STYLE OBJ!!
 
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox as tkMessageBox
+
+import dbSchema1
 
 import sys
 import requests
@@ -14,125 +21,179 @@ import base64
 root = Tk()
 usernameVal = StringVar()
 
+userUpdatenameVal = StringVar()
 
+
+db = dbSchema1.database(filename = 'timeSheets.db', table = 'Employee')
 customerList = []
+
+
+userName = "administrator:"
+password = ""
+construida = "".join((userName, password))
+encoded = base64.b64encode (bytes(construida, "utf-8"))
 
 class myclass():
 
-    def __init__(self):
-    	self.getCustomerData() #ponerle un HUD!
-    	self.main() #atencion! es un loop! se quedara aqui!
-        # en objC [self main]
+		def __init__(self):
+			self.initDB()
+			self.getCustomerData() #ponerle un HUD!
+			
+			self.main() #atencion! es un loop! se quedara aqui!
+				# en objC [self main]
 
-    def submitForm(self,*args):
-        try:
-            print("submitForm pressed")
+		def submitForm(self,*args):
+				try:
+						print("submitForm pressed")
+						customerName = self.usernameVal.get()
+						#nota GETTER N SETTER! DE EL WIDGET ATRAVEZ DE VAR!!
+						print('name is %s' %customerName ) #mira como hace el print!
+						self.usernameVal.set("")
+ 
+						#save to db!
+						db.insert(dict(name = customerName, surname = 1))
 
-            #nota GETTER N SETTER! DE EL WIDGET ATRAVEZ DE VAR!!
-            print('name is %s' % self.usernameVal.get()) #mira como hace el print!
-            self.usernameVal.set("")
+						#update MYOB
+						r = requests.post("http://localhost:8080/AccountRight/6680178c-1cb2-44b4-ab9f-8ca75155f9f5/Contact/Customer",headers={"x-myobapi-cftoken":encoded});
+						
+						print(r.text)
 
-        except ValueError:
-            pass
+						data = r.json()
 
-    def getCustomerData(self):
-    	print ("mellammmo")
-    	userName = "administrator:"
-    	password = ""
-    	construida = "".join((userName, password))
-    	print (construida)
-    	encoded = base64.b64encode (bytes(construida, "utf-8"))
-    	print (encoded)
-    	r = requests.get("http://localhost:8080/AccountRight/6680178c-1cb2-44b4-ab9f-8ca75155f9f5/Contact/Customer",headers={"x-myobapi-cftoken":encoded});
-    	data = r.json()
-    	#print(data) 
-    	#print (data["Items"] [0])
-    	#print(data["Items"])
+						print("tu status code", r.status_code)
 
-    	print("length data array: %i" %len(data))
+						#alert confirm
+						result = tkMessageBox.showinfo(title="titu", message="Customer created \n.)")
+#.showinfo(title, message, options)
 
-    	arraya = data["Items"]
-    	i = 0
-    	for itemDicto in arraya:
-    	    print ("tiu mare")
-    	    print (itemDicto["CoLastName"])
-    	    customerList.insert(i, itemDicto["CoLastName"])
-    	    i += 1
+				except ValueError:
+						pass
+
+		def updateForm(self, *args):
+			print()
+			try:
+				print("submitForm pressed")
+				print('name is %s' % self.userUpdatenameVal.get())
+				self.userUpdatenameVal.set("")
+			except ValueError:
+				pass
+
+
+		def initDB(self):
+			#db = dbSchema1.database(filename = 'timeSheets.db', table = 'Employee')
+			print('Create table Employee')
+			db.sql_do('drop table if exists Employee') #borra siempre?
+			db.sql_do('create table Employee ( name text, surname int )')
+			
+
+		def getCustomerData(self):
+
+			#test db mock!
+
+
+			print ("mellammmo")
+			#userName = "administrator:"
+			#password = ""
+			#construida = "".join((userName, password))
+			#encoded = base64.b64encode (bytes(construida, "utf-8"))
+			
+			print (construida)
+
+			print ("tu enconded", encoded)
+			r = requests.get("http://localhost:8080/AccountRight/6680178c-1cb2-44b4-ab9f-8ca75155f9f5/Contact/Customer",headers={"x-myobapi-cftoken":encoded});
+			data = r.json()
+			#print(data) 
+			#print (data["Items"] [0])
+			#print(data["Items"])
+
+			print("length data array: %i" %len(data))
+
+			arraya = data["Items"]
+			i = 0
+			for itemDicto in arraya:
+					print ("tiu mare")
+					print (itemDicto["CoLastName"])
+					customerList.insert(i, itemDicto["CoLastName"])
+
+					#insert!
+					db.insert(dict(name = itemDicto["CoLastName"], surname = 1))
+
+
+					i += 1
+
+			print('Create rows')
+			#db.insert(dict(name = 'one', surname = 1))
+			#db.insert(dict(name = 'two', surname = 2))
+			#db.insert(dict(name = 'three', surname = 3))
+			#db.insert(dict(name = 'four', surname = 4))
+			for row in db: print(row)
 
  
 
-    def test(self):
-    	print ("huhuha")
+		def test(self):
+			print ("huhuha")
 
-    def main(self):
+		def main(self):
  
-        print("from main")
+				print("from main")
 
-        #self.test
+				#self.test
 
-        print(customerList)
-    #container view
-        mainframe = Frame(root) #  mainframe contained by root!, init
+				print(customerList)
+		#container view
+				mainframe = Frame(root) #  mainframe contained by root!, init
 
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S)) #  add subview mainframe
-        mainframe.columnconfigure(0, weight=1)
-        mainframe.rowconfigure(0, weight=1)
+				mainframe.grid(column=0, row=0, sticky=(N, W, E, S)) #  add subview mainframe
+				mainframe.columnconfigure(0, weight=1)
+				mainframe.rowconfigure(0, weight=1)
 
-    #UI widgets
-        createLbl = Label(mainframe, text='Create  Update Read Delete Customer')
-        createLbl.grid(column=1 , row=0, sticky=(W,E))
+		#UI widgets
+				createLbl = Label(mainframe, text='Create  Update Read Delete Customer')
+				createLbl.grid(column=1 , row=0, sticky=(W,E))
 
-    # user name
-        nameLbl = Label(mainframe, text='New Customer')
-        nameLbl.grid(column=0 , row=1, sticky=(W,E))
+		# user name
+				nameLbl = Label(mainframe, text='New Customer')
+				nameLbl.grid(column=0 , row=1, sticky=(W,E))
 
-        self.usernameVal = StringVar()
-        usernameTf = Entry(mainframe, textvariable = self.usernameVal)
-        usernameTf.grid(column=1 , row=1, sticky=(E))
+				self.usernameVal = StringVar()
+				usernameTf = Entry(mainframe, textvariable = self.usernameVal)
+				usernameTf.grid(column=1 , row=1, sticky=(W,E))
 
-        button = Button(mainframe, text='Save', command=self.submitForm)
-        button.grid(column=3 , row=1, sticky=(W,E))  
+				button = Button(mainframe, text='Save New', command=self.submitForm)
+				button.grid(column=1, row=2, sticky=(E))  
 
-        customerLbl = Label(mainframe, text='Customers')
-        customerLbl.grid(column=0 , row=2, sticky=(W,E))
+				customerLbl = Label(mainframe, text='Update Customers')
+				customerLbl.grid(column=0 , row=3, sticky=(W,E))
 
-        l = Listbox(mainframe, height=5)
-        l.grid(column=1, row=2, sticky=(N, W, E, S))
-        s = Scrollbar(mainframe, orient=VERTICAL, command=l.yview)
-        s.grid(column=2, row=2, sticky=(N, S, W))
-        l['yscrollcommand'] = s.set
-        
+				l = Listbox(mainframe, height=5)
+				l.grid(column=1, row=3, sticky=(N, W, E, S))
+				s = Scrollbar(mainframe, orient=VERTICAL, command=l.yview)
+				s.grid(column=2, row=3, sticky=(N, S, W))
+				l['yscrollcommand'] = s.set 
+			 
+				index = 0
 
-        index = 0
+				for lana in customerList:
+					print (lana)
+					l.insert(index, lana)
+					index += 1
 
-        for lana in customerList:
-        	print (lana)
-        	l.insert(index, lana)
-        	index += 1
+				#Update
+				updateLbl = Label(mainframe, text='Edit Customer')
+				updateLbl.grid(column=0 , row=4, sticky=(W,E))
 
+				self.userUpdatenameVal = StringVar()
+				usernameUpdateTf = Entry(mainframe, textvariable = self.userUpdatenameVal)
+				usernameUpdateTf.grid(column=1 , row=4, sticky=(W,E))
 
-        #for customerObj in customerList
-            #l.insert (index, customerObj)
-            #print customerObj
-        #for i in range(1,4):
-        #	l.insert('end', 'Line %d of 100' % i)
+				saveUpdatebutton = Button(mainframe, text='Save Edit', command=self.updateForm)
+				saveUpdatebutton.grid(column=1, row=5, sticky=(E))
+				
+				
+			
 
-
-        detailsLbl = Label(mainframe, text='Details')
-        detailsLbl.grid(column=3 , row=2, sticky=(W,E))
-
-        l2 = Listbox(mainframe, height=5)
-        l2.grid(column=4, row=2, sticky=(N, W, E, S))
-        s2 = Scrollbar(mainframe, orient=VERTICAL, command=l2.yview)
-        s2.grid(column=5, row=2, sticky=(N, S))
-        l2['yscrollcommand'] = s2.set
-
-        l2.insert(0, "mashu pishu")
-        #for i in range(1,10):
-         #   l2.insert('end', 'Line %d of 100' % i)
-
-    #my loop
-        root.mainloop()
+		#my loop
+				root.mainloop()
 
 if __name__ == "__main__":
-    myclass()
+		myclass()
